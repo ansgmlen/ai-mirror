047fdd891298e6328c8d1ca031a7aac3be92299a
heedoo moon <ansgmlen@naver.com>
2017-03-25 09:26:42 -0500

    config

--- server/config.js
+++ server/config.js
@@ -2,32 +2,32 @@ var config = {
 	dev : {
 		appKey : "",
 		endpoint : "http://localhost:3000/",
-		witToken : "62FPYZSZSLZ4GPQZ7VZJYS2TQICB257R",
-		googleApiKey : "AIzaSyBP5TvGyg7kfe5KtGqkKG7jnIAlBSnB_xs",
-		darkSkyKey : "2304a2c266af319347eb8ec7b33680e5",
-		googleProjectId : "smart-mirror-160916",
-		newsapi : "4d9adae55ff048d18de8840b9cdfdcfc",
-		googleCalendarId : "heedoo21c@gmail.com"
+		witToken : "",
+		googleApiKey : "",
+		darkSkyKey : "",
+		googleProjectId : "smart-mirror-",
+		newsapi : "",
+		googleCalendarId : ""
 	},
 	prd : {
 		appKey : "",
 		endpoint : "http://localhost:3000/",
-		witToken : "62FPYZSZSLZ4GPQZ7VZJYS2TQICB257R",
-		googleApiKey : "AIzaSyBP5TvGyg7kfe5KtGqkKG7jnIAlBSnB_xs",
-		darkSkyKey : "2304a2c266af319347eb8ec7b33680e5",
-		googleProjectId : "smart-mirror-160916",
-		newsapi : "4d9adae55ff048d18de8840b9cdfdcfc",
-		googleCalendarId : "heedoo21c@gmail.com"
+		witToken : "",
+		googleApiKey : "",
+		darkSkyKey : "",
+		googleProjectId : "smart-mirror-",
+		newsapi : "",
+		googleCalendarId : ""
 	},
 	currentEnv: {
 		appKey : "",
 		endpoint : "http://localhost:3000/",
-		witToken : "62FPYZSZSLZ4GPQZ7VZJYS2TQICB257R",
-		googleApiKey : "AIzaSyBP5TvGyg7kfe5KtGqkKG7jnIAlBSnB_xs",
-		darkSkyKey : "2304a2c266af319347eb8ec7b33680e5",
-		googleProjectId : "smart-mirror-160916", // Your Google Cloud Platform project ID  this is project num: 258798813022
-		newsapi : "4d9adae55ff048d18de8840b9cdfdcfc",
-		googleCalendarId : "heedoo21c@gmail.com"
+		witToken : "",
+		googleApiKey : "",
+		darkSkyKey : "",
+		googleProjectId : "smart-mirror-", // Your Google Cloud Platform project ID  this is project num: 258798813022
+		newsapi : "",
+		googleCalendarId : ""
 	}
 }
 exports.CONFIG = config;
